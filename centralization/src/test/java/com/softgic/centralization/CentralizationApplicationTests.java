d4cd13abf077f81a899a80b553b3a255a73733cb
Cristian Mayorga <cristiandres1317@gmail.com>
2026-06-12 12:31:35 -0500

    feat: estructura inicial del proyecto

---
+++ centralization/src/test/java/com/softgic/centralization/CentralizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softgic.centralization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
